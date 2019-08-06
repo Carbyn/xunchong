470016166d914966c84f1306492c7e136cb6600a
Carbyn <carbyn.wu@gmail.com>
2019-08-06 11:37:29 +0800

    Import ali sdk

--- application/library/Explorer/Ali.php
+++ application/library/Explorer/Ali.php
@@ -10,6 +10,9 @@ class Ali {
             self::$tc = new \TopClient();
             self::$tc->appkey = $config->tbk->appkey;
             self::$tc->secretKey = $config->tbk->secretkey;
+            self::$tc->format = 'json';
+            self::$tc->connectTimeout = 1;
+            self::$tc->readTimeout = 2;
         }
         return self::$tc;
     }
--- index.php
+++ index.php
@@ -4,6 +4,11 @@ define('APPLICATION_PATH', dirname(__FILE__));
 
 require_once(APPLICATION_PATH.'/vendor/autoload.php');
 
+define('TOP_SDK_WORK_DIR', '/var/log/ali');
+define('TOP_SDK_DEV_MODE', true);
+define('TOP_AUTOLOADER_PATH', APPLICATION_PATH.'/application/library/Ali');
+require_once(APPLICATION_PATH.'/application/library/Ali/TopSdk.php');
+
 $application = new Yaf\Application(APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
