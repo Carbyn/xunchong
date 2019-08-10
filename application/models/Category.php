3ed0e3e24d71a45777e0451182589fbbda6b3327
Carbyn <carbyn.wu@gmail.com>
2019-08-11 02:00:15 +0800

    Update categories

--- application/models/Category.php
+++ application/models/Category.php
@@ -42,7 +42,7 @@ class CategoryModel extends AbstractModel {
                 foreach($categories as $ccc) {
                     $ccc = (array)$ccc;
                     if ($ccc['pcid'] == $cc['cid']) {
-                        $cc['children'][] = $ccc;
+                        $cc['children'][] = $this->format($ccc);
                     }
                 }
             }
@@ -53,6 +53,14 @@ class CategoryModel extends AbstractModel {
     public function fetchCatByPcidAndName($pcid, $name) {
         $where = compact('pcid', 'name');
         return $this->db->table(self::TABLE)->where($where)->get();
+    }
+
+    private function format($category) {
+        if ($category['icon']) {
+            $config = new \Yaf\Config\Ini(APPLICATION_PATH.'/conf/common.ini', \Constants::env());
+            $category['icon'] = $config->common->domain.$category['icon'];
+        }
+        return $category;
     }
 
 }
