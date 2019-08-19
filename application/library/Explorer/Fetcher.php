59575a4f229ecd6c4dc7cce3e6b98123d18ecd06
Carbyn <carbyn.wu@gmail.com>
2019-08-19 18:52:54 +0800

    Remove duplicate function

--- application/library/Explorer/Fetcher.php
+++ application/library/Explorer/Fetcher.php
@@ -74,37 +74,4 @@ class Fetcher {
         return false;
     }
 
-    public static function postWithRetry($url, $data, $headers = [], $retry = 3) {
-        $curl = new \Curl\Curl();
-        while ($retry > 0) {
-            if (!empty($headers)) {
-                foreach($headers as $key => $val) {
-                    switch($key) {
-                    case 'User-Agent':
-                        $curl->setUserAgent($val);
-                        break;
-                    case 'Referer':
-                        $curl->setReferer($val);
-                        break;
-                    case 'Cookie':
-                        $curl->setCookie($val['key'], $val['val']);
-                        break;
-                    case 'Accept-Encoding':
-                        $curl->setHeader($key, $val);
-                        $curl->setOpt(CURLOPT_ENCODING, $val);
-                        break;
-                    default:
-                        $curl->setHeader($key, $val);
-                    }
-                }
-            }
-            $curl->post($url, $data);
-            if (!$curl->error) {
-                return $curl->response;
-            }
-            $retry--;
-        }
-        return false;
-    }
-
 }
