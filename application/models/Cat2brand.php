677b275560f42b0f7e8d1d58e2185e2004b1ccb0
Carbyn <carbyn.wu@gmail.com>
2019-08-21 00:59:37 +0800

    Add related brands in category api

--- application/models/Cat2brand.php
+++ application/models/Cat2brand.php
@@ -12,6 +12,22 @@ class Cat2brandModel extends AbstractModel {
         return $this->db->table(self::TABLE)->where($where)->get();
     }
 
+    public function fetchBrandsByCid($cid) {
+        $where['cid'] = $cid;
+        $rows = $this->db->table(self::TABLE)->where($where)->getAll();
+        if (empty($rows)) {
+            return [];
+        }
+        $bids = [];
+        foreach($rows as $row) {
+            $bids[] = $row->bid;
+        }
+
+        $brandModel = new BrandModel();
+        $brands = $brandModel->batchFetch($bids);
+        return $brands;
+    }
+
     public function fetchAll() {
         return (array)$this->db->table(self::TABLE)->getAll();
     }
